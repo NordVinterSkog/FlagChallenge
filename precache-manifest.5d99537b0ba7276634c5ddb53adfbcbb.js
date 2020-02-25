a25e244da9269b547abf7fc2eed3f15fa3f3ca01
Jan <kamilj_95@wp.pl>
2020-02-25 14:22:05 +0100

    Updates

---
+++ precache-manifest.5d99537b0ba7276634c5ddb53adfbcbb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596cebffe60f7b03d431d1f875990302",
+    "url": "/FlagChallenge/index.html"
+  },
+  {
+    "revision": "f7a0d1a2091c77289130",
+    "url": "/FlagChallenge/static/css/main.d0bcf6c3.chunk.css"
+  },
+  {
+    "revision": "16e9c66960dc451c4515",
+    "url": "/FlagChallenge/static/js/2.c6006e05.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FlagChallenge/static/js/2.c6006e05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7a0d1a2091c77289130",
+    "url": "/FlagChallenge/static/js/main.deb3c3d6.chunk.js"
+  },
+  {
+    "revision": "21d105a445dead3baf19",
+    "url": "/FlagChallenge/static/js/runtime-main.2de952f6.js"
+  },
+  {
+    "revision": "a32a54376453f4bae15c7f5de31b820c",
+    "url": "/FlagChallenge/static/media/africa.a32a5437.png"
+  },
+  {
+    "revision": "4557159564064ed201f6f9e87fedfffd",
+    "url": "/FlagChallenge/static/media/america.45571595.png"
+  },
+  {
+    "revision": "798a84b5cd9d35eaa91280569050ea3a",
+    "url": "/FlagChallenge/static/media/asia.798a84b5.png"
+  },
+  {
+    "revision": "2bd56f28434dc279ae389641a5a0425e",
+    "url": "/FlagChallenge/static/media/australia.2bd56f28.png"
+  },
+  {
+    "revision": "6703263afa3c1d6728494f7c5282270e",
+    "url": "/FlagChallenge/static/media/earth.6703263a.png"
+  },
+  {
+    "revision": "921bbe3676cd233f2d9307aba500bcc0",
+    "url": "/FlagChallenge/static/media/europe.921bbe36.png"
+  },
+  {
+    "revision": "aacbc63ccf5245fc55ec69ac397794d3",
+    "url": "/FlagChallenge/static/media/world.aacbc63c.png"
+  }
+]);
